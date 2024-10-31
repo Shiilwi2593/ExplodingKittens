3c0f27a9f0c8aa2637a6a7117ebf17a73a5bab48
graylwi2593 <graylwi2593@gmail.com>
2024-10-31 15:11:47 +0700

    Initial Commit

---
+++ Exploding Kittens/Exploding_KittensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploding_KittensApp.swift
+//  Exploding Kittens
+//
+//  Created by Trịnh Kiết Tường on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Exploding_KittensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
