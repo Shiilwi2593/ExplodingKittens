0188954ff2f14a09403a3dc3ec3b638d3acabcec
graylwi2593 <graylwi2593@gmail.com>
2024-11-01 23:26:08 +0700

    Game Lobby, Game PlayView

---
+++ Exploding Kittens/Extension/ColorExtend.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtend.swift
+//  Exploding Kittens
+//
+//  Created by Trịnh Kiết Tường on 1/11/24.
+//
+
+import Foundation
---
+++ Exploding Kittens/View/Game/GamePlayView.swift
@@ -0,0 +1,18 @@
+//
+//  GamePlayView.swift
+//  Exploding Kittens
+//
+//  Created by Trịnh Kiết Tường on 1/11/24.
+//
+
+import SwiftUI
+
+struct GamePlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamePlayView()
+}
